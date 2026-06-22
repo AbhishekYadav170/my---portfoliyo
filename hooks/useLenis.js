42e26c03274fd65ea68e375eea2a263dd9fea3ef
sethyadav <sethyadav169@gmail.com>
2026-06-22 11:59:43 +0530

    portfoliyo

---
+++ hooks/useLenis.js
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function useLenis() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    // Sync Lenis with ScrollTrigger
+    lenis.on("scroll", ScrollTrigger.update);
+
+    // Use GSAP ticker instead of requestAnimationFrame
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+
+    // Refresh after everything is ready
+    ScrollTrigger.refresh();
+
+    return () => {
+      gsap.ticker.remove((time) => {
+        lenis.raf(time * 1000);
+      });
+
+      lenis.destroy();
+      ScrollTrigger.killAll();
+    };
+  }, []);
+}
